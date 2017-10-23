4f3f9203ab3dc8dd35e9984f587d8a5da95dad2e
Nathan Barrett and Shane Ryan <nathan@>
2017-10-23 11:34:49 -0700

    add loops to forms

--- fav-things/js/scripts.js
+++ fav-things/js/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
   $("#secrets").submit(function(event) {
     event.preventDefault();
-    var nameInput = $('input#nameInput').val();
-    var foodInput = $('input#foodInput').val();
-    var hobbyInput = $('input#hobbyInput').val();
-    var animalInput = $('input#animalInput').val();
-    var sportsballInput = $('input#teamInput').val();
-    var favArray = [nameInput, foodInput, hobbyInput, animalInput, sportsballInput];
-    console.log(favArray.toString());
-    var newArray = []
-    newArray.push(favArray[1]);
-    newArray.push(favArray[0]);
-    newArray.push(favArray[2]);
-    console.log(newArray.toString());
-    $('#liOne').text(newArray[0]);
-    $('#liTwo').text(newArray[1]);
-    $('#liThree').text(newArray[2]);
+    var inputs = ["nameInput", "foodInput", "hobbyInput", "animalInput", "sportsballInput"]
+    $(".hide").show();
+    inputs.forEach(function(input) {
+      var userInput = $("input#" + input).val();
+      $("." + input).text(userInput);
+      // var nameInput = $('input#nameInput').val();
+      // var foodInput = $('input#foodInput').val();
+      // var hobbyInput = $('input#hobbyInput').val();
+      // var animalInput = $('input#animalInput').val();
+      // var sportsballInput = $('input#sportsballInput').val();
+      // var favArray = [nameInput, foodInput, hobbyInput, animalInput, sportsballInput];
+      // console.log(favArray.toString());
+    });
+
 
   });
 });
