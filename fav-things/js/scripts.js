70ec6f7aab920aebc2b3bb15fc8a654d0ba87c6b
Nathan Barrett and Shane Ryan <nathan@>
2017-10-23 10:19:59 -0700

    add scripts.js that makes two seperate arrays

---
+++ fav-things/js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#secrets").submit(function(event) {
+    event.preventDefault();
+    var nameInput = $('input#nameInput').val();
+    var foodInput = $('input#foodInput').val();
+    var hobbyInput = $('input#hobbyInput').val();
+    var animalInput = $('input#animalInput').val();
+    var sportsballInput = $('input#teamInput').val();
+    var favArray = [nameInput, foodInput, hobbyInput, animalInput, sportsballInput];
+    console.log(favArray.toString());
+    var newArray = []
+    newArray.push(favArray[1]);
+    newArray.push(favArray[0]);
+    newArray.push(favArray[2]);
+    console.log(newArray.toString());
+    debugger;
+  });
+});
